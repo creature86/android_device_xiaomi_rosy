88c0081f7e9aa742308a729583085f0367e5f39e
Quallenauge <Hamsi2k@freenet.de>
2019-04-16 23:02:39 +0200

    rosy: Add configfs support for 4.9 kernels
     - also update the commandline to set androidboot.usbconfigfs for kernel 4.9 for USB enumeration to work properly.
    
    Change-Id: Icb006e561c427060adc78da505640b8cab8d914e

--- device.mk
+++ device.mk
@@ -314,6 +314,9 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.goodix.sh \
+    init.msm.usb.configfs.rc \
+    init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
